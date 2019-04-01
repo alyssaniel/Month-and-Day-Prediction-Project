53ac17f92b80086f8d0a684b19f0941c6e7a8a3d
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-04-01 07:49:31 -0400

    Added Reminders

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -4,5 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+    // Create a method for returning the number of days in their birth month and the written form of their birth month.
+    // Create a method for determining what day of the week the user was born on based on previously given input.
+    // Create a method that will return the line of the poem that fits their birthday.
+    // Need 5 commits
     }
 }
