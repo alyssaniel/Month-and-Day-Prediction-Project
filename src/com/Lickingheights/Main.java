b071f11dd030045fede686c49b5860e042d239ad
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-04-04 08:09:50 -0400

    Added Keyboard and Prints

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -1,5 +1,6 @@
 package com.Lickingheights;
 
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
@@ -8,5 +9,25 @@ public class Main {
     // Create a method for determining what day of the week the user was born on based on previously given input.
     // Create a method that will return the line of the poem that fits their birthday.
     // Need 5 commits
+       //Scanners
+
+        Scanner keyboard = new Scanner(System.in);
+
+
+        System.out.print("Enter what day you were born on:(Enter Number)");
+        int dayfMonth = keyboard.nextInt();
+        System.out.print("Enter Month:(Enter Number");
+        int month = keyboard.nextInt();
+        System.out.print("Enter Year");
+        int year = keyboard.nextInt();
+
+
+
+
+
+
+
+
+
     }
 }
