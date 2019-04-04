e478d53d2371faaefab62faa6a3f86b79a00b283
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-04-04 08:23:48 -0400

    Created The Second Method

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -4,12 +4,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    // Create a method for returning the number of days in their birth month and the written form of their birth month.
-    // Create a method for determining what day of the week the user was born on based on previously given input.
-    // Create a method that will return the line of the poem that fits their birthday.
-    // Need 5 commits
-       //Scanners
+        // write your code here
+        // Create a method for returning the number of days in their birth month and the written form of their birth month.
+        // Create a method for determining what day of the week the user was born on based on previously given input.
+        // Create a method that will return the line of the poem that fits their birthday.
+        // Need 5 commits
 
         Scanner keyboard = new Scanner(System.in);
 
@@ -22,14 +21,26 @@ public class Main {
         int year = keyboard.nextInt();
 
 
-        if (month ==1 || month == 2)
-        {
+        if (month == 1 || month == 2) {
 
-            month = (month == 1) ? 13
-                    year--;
+            month = (month == 1) ? 13 : 14;
+            year--;
 
         }
-            
+
+
+        {
+        int dayOfWeek = (dayOfMonth + (26 * (month + 1)) / 10 + (year % 100)
+                + year (year % 100) / 4 + (year / 100) / 4 + 5 * (year / 100)) % 7;
+
+
+        System.out.print("Day of the week is");
+        switch(dayOfWeek)
+            }
+
+
+
+
 
 
 
