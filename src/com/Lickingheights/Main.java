a0f81d4ebe2902b147508bacf99d64053378685f
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-04-05 08:12:59 -0400

    Added All The Cases and Breaks

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -15,7 +15,7 @@ public class Main {
 
         System.out.print("Enter what day you were born on:(Enter Number)");
         int dayOfMonth = keyboard.nextInt();
-        System.out.print("Enter Month:(Enter Number");
+        System.out.print("Enter Month:(Enter Number)");
         int month = keyboard.nextInt();
         System.out.print("Enter Year");
         int year = keyboard.nextInt();
@@ -29,13 +29,45 @@ public class Main {
         }
 
 
-        {
-        int dayOfWeek = (dayOfMonth + (26 * (month + 1)) / 10 + (year % 100)
-                + year (year % 100) / 4 + (year / 100) / 4 + 5 * (year / 100)) % 7;
+
+            int dayOfWeek = (dayOfMonth + (26 * (month + 1)) / 10 + (year % 100)
+                    + year(year % 100) / 4 + (year / 100) / 4 + 5 * (year / 100)) % 7;
 
 
-        System.out.print("Day of the week is");
-        switch(dayOfWeek)
+            System.out.print("Day of the week is");
+            switch (dayOfWeek)
+
+            {
+                case 1:
+                    System.out.println("You were born on a Saturday");
+                    System.out.println("Saturdays Child Work Hard For Their Living");
+                    break;
+                case 2:
+                    System.out.print("You were born on a Sunday");
+                    System.out.print("The Child That is Born On The Sabbath Day Is " +
+                            "Fair And Wise And Good In Every Way");
+                    break;
+                case 3:
+                    System.out.print("You were born on a Monday");
+                    System.out.print("Mondays Child Is Fair Of Face");
+                    break;
+                case 4:
+                    System.out.print("You were born on a Tuesday");
+                    System.out.print("Tuesday Child Is Full of Grace");
+                    break;
+                case 5:
+                    System.out.print("You were bron on a Wednesday");
+                    System.out.print("Wednesdays Child Is Full Of Woe");
+                    break;
+                case 6:
+                    System.out.print("You were born on a Thursday");
+                    System.out.print("Thursday Child Has Far To Go");
+                    break;
+                case 7:
+                    System.out.print("You were born on a Friday");
+                    System.out.print("Fridays Child Is Loving And Giving");
+                    break;
+
             }
 
 
@@ -46,8 +78,7 @@ public class Main {
 
 
 
-
-
-
     }
+
+
 }
