aa480cae48ae9c18b75ca3e2479620303edbcf24
Alyssa <alyssa.nichols.2022@student.lhschools.org>
2019-04-16 08:02:40 -0400

    Added A Loop

--- src/com/Lickingheights/Main.java
+++ src/com/Lickingheights/Main.java
@@ -11,26 +11,31 @@ public class Main {
         // Need 5 commits
 
         Scanner keyboard = new Scanner(System.in);
+        Scanner continueKB;
+        continueKB = new Scanner(System.in);
+        String keepGoing;
+        int go = 0;
 
-        System.out.println("Enter Month:(Enter Number)");
+        while (go == 0) {
+
+        System.out.println("Enter Month(Enter Number)");
         int month = keyboard.nextInt();
-        System.out.println("Enter what day you were born on:(Enter Number)");
+        System.out.println("Enter what day you were born on");
         int dayOfMonth = keyboard.nextInt();
         System.out.println("Enter Year");
         int year = keyboard.nextInt();
 
 
-        if (month == 1 || month == 2) {
+            if (month == 1 || month == 2) {
 
-            month = (month == 1) ? 13 : 14;
-            year--;
+                month = (month == 1) ? 13 : 14;
+                year--;
 
-        }
+            }
 
 
-
-        int dayOfWeek = (dayOfMonth + (26 * (month + 1)) / 10 + (year % 100)
-                +  (year % 100) / 4 + (year / 100) / 4 + 5 * (year / 100)) % 7;
+            int dayOfWeek = (dayOfMonth + (26 * (month + 1)) / 10 + (year % 100)
+                    + (year % 100) / 4 + (year / 100) / 4 + 5 * (year / 100)) % 7;
 
 
             System.out.print("You were born on a ");
@@ -39,44 +44,46 @@ public class Main {
             {
                 case 0:
                     System.out.println("Saturday");
-                    System.out.print("Saturdays Child Work Hard For Their Living");
+                    System.out.println("Saturdays Child Work Hard For Their Living");
                     break;
                 case 1:
                     System.out.println("Sunday");
-                    System.out.print("The Child That is Born On The Sabbath Day Is " +
+                    System.out.println("The Child That is Born On The Sabbath Day Is " +
                             "Fair And Wise And Good In Every Way");
                     break;
                 case 2:
                     System.out.println("Monday");
-                    System.out.print("Mondays Child Is Fair Of Face");
+                    System.out.println("Mondays Child Is Fair Of Face");
                     break;
                 case 3:
                     System.out.println("Tuesday");
-                    System.out.print("Tuesday Child Is Full of Grace");
+                    System.out.println("Tuesday Child Is Full of Grace");
 
                     break;
                 case 4:
                     System.out.println("Wednesday");
-                    System.out.print("Wednesdays Child Is Full Of Woe");
+                    System.out.println("Wednesdays Child Is Full Of Woe");
 
                     break;
                 case 5:
                     System.out.println("Thursday");
-                    System.out.print("Thursday's child has far to go.");
+                    System.out.println("Thursday's child has far to go.");
                     break;
                 case 6:
                     System.out.println("Friday");
-                    System.out.print("Fridays Child Is Loving And Giving");
+                    System.out.println("Fridays Child Is Loving And Giving");
                     break;
 
             }
 
 
+            System.out.println("Would you like to play again?");
+            System.out.println("Type 'y' for yes or 'n' for no");
+            keepGoing = continueKB.nextLine();
+            go = keepGoing.indexOf("y");
 
 
-
-
-
+        }
 
     }
 
